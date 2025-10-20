535ea0d7bb49ef93cb8eceddb2ab3cc8b35deff5
Fares Quiqflow <fares@quiqflow.com>
2025-10-20 14:38:56 +0300

    Refactor example.js for consistent indentation and minor text update in greet function

--- example.js
+++ example.js
@@ -7,30 +7,30 @@ var isDeveloper = true;
 
 // 2. Function
 function greet(userName) {
-    return `Hello, ${userName}! Welcome to JavaScript.`;
+  return `Hello, ${userName}! Welcome to JavaScript. edit`;
 }
 
 // 3. Object
 const user = {
-    name: "Fares",
-    age: 25,
-    skills: ["JavaScript", "Node.js", "React"],
-    greet: function() {
-        console.log(`Hi, I'm ${this.name} and I know ${this.skills.join(", ")}.`);
-    }
+  name: "Fares",
+  age: 25,
+  skills: ["JavaScript", "Node.js", "React"],
+  greet: function () {
+    console.log(`Hi, I'm ${this.name} and I know ${this.skills.join(", ")}.`);
+  },
 };
 
 // 4. Loop
 console.log("Counting from 1 to 5:");
 for (let i = 1; i <= 5; i++) {
-    console.log(i);
+  console.log(i);
 }
 
 // 5. Conditional
 if (isDeveloper) {
-    console.log(greet(name));
+  console.log(greet(name));
 } else {
-    console.log("You should learn JavaScript!");
+  console.log("You should learn JavaScript!");
 }
 
 // 6. Using the object method
